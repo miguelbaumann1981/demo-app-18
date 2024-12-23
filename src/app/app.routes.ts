0861491395e09bf4b8d7df75c28978e804458d1c
MiguelDev <miguelmartinezm@outlook.es>
2024-12-23 19:50:36 +0100

    settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./modules/dashboard/pages/dps-page-dashboard/dps-page-dashboard.component').then(m => m.DpsPageDashboardComponent)
+        loadComponent: () => import('@dashboard/pages/dps-page-dashboard/dps-page-dashboard.component').then(m => m.DpsPageDashboardComponent)
     },
     {
         path: 'factoryConfigurator',
-        loadComponent: () => import('./modules/factory-configurator/pages/dps-page-factory-configurator/dps-page-factory-configurator.component').then(m => m.DpsPageFactoryConfiguratorComponent)
+        loadComponent: () => import('@factoryConfigurator/pages/dps-page-factory-configurator/dps-page-factory-configurator.component').then(m => m.DpsPageFactoryConfiguratorComponent)
     },
     {
         path: '**',
